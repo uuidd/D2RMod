0db2a5197cf64450d1a7e7965b2494d4b1b50e99
demo <1942125235@qq.com>
2023-05-24 00:14:22 +0800

    刺客特效修改及百科全书增加

--- 刺客特效修改/mod.js
+++ 刺客特效修改/mod.js
@@ -1,5 +1,5 @@
 D2RMM.copyFile(
-  'hd', // <mod folder>\hd
-  'hd', // <diablo 2 folder>\mods\<modname>\<modname>.mpq\data\hd
+  'data', // <mod folder>\hd
+  '', // <diablo 2 folder>\mods\<modname>\<modname>.mpq\data\hd
   true // overwrite any conflicts
 );
--- 暗黑2百科全书/mod.js
+++ 暗黑2百科全书/mod.js
@@ -1,5 +1,5 @@
 D2RMM.copyFile(
-  'global', // <mod folder>\hd
-  'global', // <diablo 2 folder>\mods\<modname>\<modname>.mpq\data\hd
+  'data', // <mod folder>\hd
+  '', // <diablo 2 folder>\mods\<modname>\<modname>.mpq\data\hd
   true // overwrite any conflicts
 );
