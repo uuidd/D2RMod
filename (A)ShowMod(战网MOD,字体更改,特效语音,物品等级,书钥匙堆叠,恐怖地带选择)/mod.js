f0bec0268f7ae2a741df17b6bfe7489c8075f746
demo <liyihuiid@gmail.com>
2023-06-15 15:25:52 +0800

    新增 lil174 作者整合mod https://bbs.3dmgame.com/thread-6420759-1-1.html

---
+++ (A)ShowMod(战网MOD,字体更改,特效语音,物品等级,书钥匙堆叠,恐怖地带选择)/mod.js
@@ -0,0 +1,264 @@
+// D2RMM.copyFile 3个参数的相关解释
+// <mod folder>\hd
+// <diablo 2 folder>\mods\<modname>\<modname>.mpq\data\hd
+// overwrite any conflicts
+
+function ModFontAndHelp()
+{
+    // 字体更改
+    switch (config.fonts_select)
+    {
+        case 'mod_1':
+            D2RMM.copyFile('fonts\\1', 'hd\\ui\\fonts', true);
+            break;
+        case 'mod_2':
+            D2RMM.copyFile('fonts\\2', 'hd\\ui\\fonts', true);
+            break;
+        case 'mod_3':
+            D2RMM.copyFile('fonts\\3', 'hd\\ui\\fonts', true);
+            break;
+        case 'mod_4':
+            D2RMM.copyFile('fonts\\4', 'hd\\ui\\fonts', true);
+            break;
+        case 'mod_5':
+            D2RMM.copyFile('fonts\\5', 'hd\\ui\\fonts', true);
+            break;
+    }
+    // jgmod帮助信息
+    if (config.jgmod_help)
+    {
+        D2RMM.copyFile('jgmodhelp\\global', 'global', true);
+        D2RMM.copyFile('jgmodhelp\\hd', 'hd', true);
+        D2RMM.copyFile('jgmodhelp\\Local', 'Local', true);
+    }
+}
+
+// 选择战网MOD
+// 注意copyFile会智能识别替换文件删除文件
+// 所以当还原为官方默认时无须MOD作者,自行备份原官方文件
+switch (config.talk_mod_select)
+{
+    case 'mod_def':
+        ModFontAndHelp();
+        break;
+    case 'mod_wing':
+        D2RMM.copyFile('wingmod1.12.0\\global', 'global', true);
+        D2RMM.copyFile('wingmod1.12.0\\hd', 'hd', true);
+        D2RMM.copyFile('wingmod1.12.0\\Local', 'Local', true);
+        ModFontAndHelp();
+        break;
+    case 'mod_yte':
+        D2RMM.copyFile('ytemod2.10\\global', 'global', true);
+        D2RMM.copyFile('ytemod2.10\\hd', 'hd', true);
+        D2RMM.copyFile('ytemod2.10\\Local', 'Local', true);
+        break;
+}
+
+
+// jgmod彩色词缀
+if (config.jgmod_color)
+{
+    D2RMM.copyFile('jgmodcolor\\global', 'global', true);
+    D2RMM.copyFile('jgmodcolor\\local', 'local', true);
+}
+
+// Buff技能结束后的语音提示
+function SoundsPushData(sounds, Sound, index, Channel, FileName)
+{
+    sounds.rows.push({
+        'Sound': Sound,
+        '*Index': index,
+        'Redirect': '',
+        'Channel': Channel,
+        'FileName': FileName,
+        'IsLocal': 0,
+        'IsMusic': 0,
+        'IsAmbientScene': 0,
+        'IsAmbientEvent': 0,
+        'IsUI': 0,
+        'Volume Min': 255,
+        'Volume Max': 255,
+        'Pitch Min': 100,
+        'Pitch Max': 100,
+        'Group Size': 0,
+        'Group Weight': 0,
+        'Loop': 0,
+        'Fade In': 0,
+        'Fade Out': 0,
+        'Defer Inst': 0,
+        'Stop Inst': 0,
+        'Duration': 0,
+        'Compound': 0,
+        'Falloff': 0,
+        'LFEMix': 15,
+        '3dSpread': 90,
+        'Priority': 255,
+        'Stream': 0,
+        'Is2D': 0,
+        'Tracking': 1,
+        'Solo': 1,
+        'Music Vol': 0,
+        'Block 1': -1,
+        'Block 2': -1,
+        'Block 3': -1,
+        'HDOptOut': 0,
+        'Delay': 0,
+        '4737': ''
+    });
+}
+
+function StatesMod(row,id,offsound)
+{
+    if (row['*ID'] == id) row.offsound = offsound;
+}
+
+if (config.skillOff_sound)
+{
+    // 16个语音文件复制
+    D2RMM.copyFile('skill', 'hd\\global\\sfx\\skill', true);
+
+    // sounds文件新增16行,来自jgmod
+    const soundsFilename = 'global\\excel\\sounds.txt';
+    const sounds = D2RMM.readTsv(soundsFilename);
+    SoundsPushData(sounds, 'chillingarmor_off', 11399, 'sfx/items_hd', 'skill\\jg\\chillingarmor_off.flac');
+    SoundsPushData(sounds, 'battleorders_off', 11400, 'sfx/items_hd', 'skill\\jg\\battleorders_off.flac');
+    SoundsPushData(sounds, 'venom_off', 11401, 'sfx/items_hd', 'skill\\jg\\venom_off.flac');
+    SoundsPushData(sounds, 'energyshield_off', 11402, 'sfx/items_hd', 'skill\\jg\\energyshield_off.flac');
+    SoundsPushData(sounds, 'frozenarmor_off', 11403, 'sfx/items_hd', 'skill\\jg\\frozenarmor_off.flac');
+    SoundsPushData(sounds, 'enchant_off', 11404, 'sfx/items_hd', 'skill\\jg\\enchant_off.flac');
+    SoundsPushData(sounds, 'holyshield_off', 11405, 'sfx/items_hd', 'skill\\jg\\holyshield_off.flac');
+    SoundsPushData(sounds, 'morph_off', 11406, 'sfx/items_hd', 'skill\\jg\\morph_off.flac');
+    SoundsPushData(sounds, 'shout_off', 11407, 'sfx/items_hd', 'skill\\jg\\shout_off.flac');
+    SoundsPushData(sounds, 'battlecommand_off', 11408, 'sfx/items_hd', 'skill\\jg\\battlecommand_off.flac');
+    SoundsPushData(sounds, 'bonearmor_off', 11409, 'sfx/items_hd', 'skill\\jg\\bonearmor_off.flac');
+    SoundsPushData(sounds, 'cyclonearmor_off', 11410, 'sfx/items_hd', 'skill\\jg\\cyclonearmor_off.flac');
+    SoundsPushData(sounds, 'shiverarmor_off', 11411, 'sfx/items_hd', 'skill\\jg\\shiverarmor_off.flac');
+    SoundsPushData(sounds, 'fade_off', 11412, 'sfx/items_hd', 'skill\\jg\\fade_off.flac');
+    SoundsPushData(sounds, 'burstofspeed_off', 11413, 'sfx/items_hd', 'skill\\jg\\burstofspeed_off.flac');
+    SoundsPushData(sounds, 'bladeshield_off', 11414, 'sfx/items_hd', 'skill\\jg\\bladeshield_off.flac');
+    D2RMM.writeTsv(soundsFilename, sounds);
+
+    // states.txt文件16个效果修改让声音生效
+    const statesFilename = 'global\\excel\\states.txt';
+    const states = D2RMM.readTsv(statesFilename);
+    states.rows.forEach((row) =>
+    {
+        StatesMod(row, 20, 'chillingarmor_off');//寒冰甲
+        StatesMod(row, 32, 'battleorders_off'); //战斗命令
+        StatesMod(row, 31, 'venom_off');        //淬毒
+        StatesMod(row, 30, 'energyshield_off'); //能量护盾
+        StatesMod(row, 10, 'frozenarmor_off');  //冰封甲
+        StatesMod(row, 16, 'enchant_off');      //附魔
+        StatesMod(row, 101, 'holyshield_off');  //神圣之盾
+        StatesMod(row, 139, 'morph_off');       //变形术(变狼)
+        StatesMod(row, 140, 'morph_off');       //变形术(变熊)
+        StatesMod(row, 26, 'shout_off');        //大吼
+        StatesMod(row, 51, 'battlecommand_off');//战斗指挥
+        StatesMod(row, 14, 'bonearmor_off');    //骸骨护甲
+        StatesMod(row, 151, 'cyclonearmor_off');//气旋护甲
+        StatesMod(row, 88, 'shiverarmor_off');  //碎冰甲
+        StatesMod(row, 159, 'fade_off');        //影散
+        StatesMod(row, 157, 'burstofspeed_off');//速度爆发
+        StatesMod(row, 158, 'bladeshield_off'); //利刃之盾
+    });
+    D2RMM.writeTsv(statesFilename, states);
+}
+
+// 显示物品等级
+if (config.Show_level)
+{
+    // 显示武器等级
+    const weaponsFilename = 'global\\excel\\weapons.txt';
+    const weapons = D2RMM.readTsv(weaponsFilename);
+    weapons.rows.forEach((row) =>
+    {
+        if (
+            // don't modify throwing potions (gas, oil pots)
+            row.type !== 'tpot'
+        )
+        {
+            row.ShowLevel = 1;
+        }
+    });
+    D2RMM.writeTsv(weaponsFilename, weapons);
+
+    // 显示护甲等级
+    const armorFilename = 'global\\excel\\armor.txt';
+    const armor = D2RMM.readTsv(armorFilename);
+    armor.rows.forEach((row) =>
+    {
+        row.ShowLevel = 1;
+    });
+    D2RMM.writeTsv(armorFilename, armor);
+
+    // 显示项链戒指珠宝板子等级
+    const miscFilename = 'global\\excel\\misc.txt';
+    const misc = D2RMM.readTsv(miscFilename);
+    misc.rows.forEach((row) =>
+    {
+        if (['amu', 'rin'].indexOf(row.code) !== -1)
+        {
+            row.ShowLevel = 1;
+        }
+        if (['cm1', 'cm2', 'cm3'].indexOf(row.code) !== -1)
+        {
+            row.ShowLevel = 1;
+        }
+        if (['jew'].indexOf(row.code) !== -1)
+        {
+            row.ShowLevel = 1;
+        }
+    });
+    D2RMM.writeTsv(miscFilename, misc);
+}
+
+// 书钥匙堆叠大小
+const miscFilename = 'global\\excel\\misc.txt';
+const misc = D2RMM.readTsv(miscFilename);
+misc.rows.forEach((row) =>
+{
+    if (
+        row.code === 'tbk' || // Tome of Town Portal,默认20
+        row.code === 'ibk' // Tome of Identify,默认20
+    )
+    {
+        row.maxstack = config.tomes_stacks;
+    }
+    if (
+        row.code === 'key' // Key,默认12
+    )
+    {
+        row.maxstack = config.keys_stacks;
+    }
+});
+D2RMM.writeTsv(miscFilename, misc);
+
+// 地图的编号请参考levels.txt文件
+switch (config.zone_select)
+{
+    case 'mod_all_map':
+        D2RMM.copyFile('zoneAllMap', 'hd\\global\\excel', true);
+        break;
+    case 'mod_all_ActMap':
+        D2RMM.copyFile('zoneAllActMap', 'hd\\global\\excel', true);
+        break;
+    case 'mod_4_map':
+        D2RMM.copyFile('zone4Map', 'hd\\global\\excel', true);
+        break;
+    case 'mod_only_a1':
+        D2RMM.copyFile('zoneOnlyA1', 'hd\\global\\excel', true);
+        break;
+    case 'mod_only_a2':
+        D2RMM.copyFile('zoneOnlyA2', 'hd\\global\\excel', true);
+        break;
+    case 'mod_only_a3':
+        D2RMM.copyFile('zoneOnlyA3', 'hd\\global\\excel', true);
+        break;
+    case 'mod_only_a4':
+        D2RMM.copyFile('zoneOnlyA4', 'hd\\global\\excel', true);
+        break;
+    case 'mod_only_a5':
+        D2RMM.copyFile('zoneOnlyA5', 'hd\\global\\excel', true);
+        break;
+
+}
---
+++ CubeMod(单机方块合成与打孔公式)/mod.js
@@ -0,0 +1,174 @@
+﻿// 添加单机公式
+const cubemainFileName = 'global\\excel\\cubemain.txt';
+const cubemain = D2RMM.readTsv(cubemainFileName);
+
+// 奶牛关=1本回城书tbk,op=28代表有条件限制
+if (config.simple_cow) PushData(`tbk=Cow Portal`, 28, 1, 'tbk', '', '', '', `Cow Portal`, '', '', '', '', '');
+
+// A2任务道具=辩识书+2#符文,op=28代表有条件限制
+// 有个注意的地方,当你身上本身带有零件时比如蛇项链，直接简化合成会无效还会消耗掉(辩识书+2#符文)，和op没关系
+if (config.a2task_item) PushData(`ibk+2rune=Horadric Staff`, 28, 2, 'ibk', 'r02', '', '', 'hst', '', '', '', '', '');
+
+// A3任务道具=辩识书+3#符文,op=28代表有条件限制
+// 有个注意的地方,当你身上本身带有零件时比如眼球，直接简化合成会无效还会消耗掉(辩识书+3#符文)，和op没关系
+if (config.a3task_item) PushData(`ibk+3rune=Khalim's Will`, 28, 2, 'ibk', 'r03', '', '', 'qf2', '', '', '', '', '');
+
+// 白装+溶解=亮金装备(模拟恰西任务)
+// lvl = 直接生成的物品等级
+// plvl = 与角色等级相乘的百分比(50就相当于角色等级*0.5)
+// ilvl = 与物品等级相乘的百分比(50就相当于原物品等级*0.5)
+// 最后生成的物品等级=lvl+(plvl*)+(ilvl*)(不太确定但是应该差不多这个意思)
+// 此处还有个坑要注意!!!
+// 如果写的"armo,nos"的话是对所有颜色的无孔装备都生效
+// 如果写的"armo,nor,hiq,nos"的话是只对超强的白色生效等同于"armo,hiq,nos",普通的白色无效
+// <恰西任务>官方设定支持超强,支持无形,超强15ED或+技能的镶材属性能被洗掉，无形的不会被洗成有形
+// <恰西任务>官方设定为需要无孔的原因，因为(有孔)是一种词缀可能会被洗掉!
+// <恰西任务>官方设定注入后生成的物品等级=角色等级+4(但是这里无法实现这种效果)
+// 注意注入后的等级一定要以人物为准不能是以原物品等级为准,因为亮金头环需要90级以上才能出+2技能
+// 而90级以上的白装是很难获得的,所以改为以人物等级为准就比较容易了
+if (config.item_rar)
+{
+    PushData(`Nor Weapo To Rar`, '', 2, `"weap,nor,nos,noe"`, 'wms', '', '', `"usetype,rar"`, 100, '', '', '', '');
+    PushData(`Sup Weapo To Rar`, '', 2, `"weap,hiq,nos,noe"`, 'wms', '', '', `"usetype,rar"`, 100, '', '', '', '');
+    PushData(`Nor Armor To Rar`, '', 2, `"armo,nor,nos,noe"`, 'wms', '', '', `"usetype,rar"`, 100, '', '', '', '');
+    PushData(`Sup Armor To Rar`, '', 2, `"armo,hiq,nos,noe"`, 'wms', '', '', `"usetype,rar"`, 100, '', '', '', '');
+
+    PushData(`Nor Eth Weapo To Eth Rar`, '', 2, `"weap,nor,nos,eth"`, 'wms', '', '', `"usetype,rar,eth"`, 100, '', '', '', '');
+    PushData(`Sup Eth Weapo To Eth Rar`, '', 2, `"weap,hiq,nos,eth"`, 'wms', '', '', `"usetype,rar,eth"`, 100, '', '', '', '');
+    PushData(`Nor Eth Armor To Eth Rar`, '', 2, `"armo,nor,nos,eth"`, 'wms', '', '', `"usetype,rar,eth"`, 100, '', '', '', '');
+    PushData(`Sup Eth Armor To Eth Rar`, '', 2, `"armo,hiq,nos,eth"`, 'wms', '', '', `"usetype,rar,eth"`, 100, '', '', '', '');
+}
+
+// armo 就包含了头衣盾
+// 此处还有个坑要注意!!!
+// 如果写的"armo,nos"的话是对所有颜色的无孔装备都生效
+// 如果写的"armo,nor,hiq,nos"的话是只对超强的白色生效等同于"armo,hiq,nos",普通的白色无效
+// 公式=(白)普通装备+完美钻,直接最大孔,仿拉苏克任务 (想打随机孔可以用原公式)
+// 公式=(蓝)魔法装备+完美钻,孔2
+// 公式=(黄)稀有装备+完美钻,孔1
+// 公式=(暗金)独特装备+完美钻,孔1
+// 公式=(绿)套装装备+完美钻,孔1
+// 公式=(橙)手工装备+完美钻,孔1
+// 注意了洗板子的公式改为1颗完美宝石时会和蓝装打孔公式冲突导致蓝开不了孔！
+// 统一使用`"useitem,sock=6"`可以固定一种颜色宝石开孔就行了,蓝装固定为2孔就行了没必要肝
+if (config.socket_item) 
+{
+    PushData(`Weap Sock Nor`, '', 2, `"weap,nor,nos"`, 'gpw', '', '', `"useitem,sock=6"`, '', '', '', '', '');
+    PushData(`Weap Sock Hiq`, '', 2, `"weap,hiq,nos"`, 'gpw', '', '', `"useitem,sock=6"`, '', '', '', '', '');
+    PushData(`Weap Sock Mag`, '', 2, `"weap,mag,nos"`, 'gpw', '', '', `"useitem,sock=2"`, '', '', '', '', '');
+    PushData(`Weap Sock Rar`, '', 2, `"weap,rar,nos"`, 'gpw', '', '', `"useitem,sock=1"`, '', '', '', '', '');
+    PushData(`Weap Sock Uni`, '', 2, `"weap,uni,nos"`, 'gpw', '', '', `"useitem,sock=1"`, '', '', '', '', '');
+    PushData(`Weap Sock Set`, '', 2, `"weap,set,nos"`, 'gpw', '', '', `"useitem,sock=1"`, '', '', '', '', '');
+    PushData(`Weap Sock Crf`, '', 2, `"weap,crf,nos"`, 'gpw', '', '', `"useitem,sock=1"`, '', '', '', '', '');
+
+    PushData(`Armo Sock Nor`, '', 2, `"armo,nor,nos"`, 'gpw', '', '', `"useitem,sock=4"`, '', '', '', '', '');
+    PushData(`Armo Sock Hiq`, '', 2, `"armo,hiq,nos"`, 'gpw', '', '', `"useitem,sock=4"`, '', '', '', '', '');
+    PushData(`Armo Sock Mag`, '', 2, `"armo,mag,nos"`, 'gpw', '', '', `"useitem,sock=2"`, '', '', '', '', '');
+    PushData(`Armo Sock Rar`, '', 2, `"armo,rar,nos"`, 'gpw', '', '', `"useitem,sock=1"`, '', '', '', '', '');
+    PushData(`Armo Sock Uni`, '', 2, `"armo,uni,nos"`, 'gpw', '', '', `"useitem,sock=1"`, '', '', '', '', '');
+    PushData(`Armo Sock Set`, '', 2, `"armo,set,nos"`, 'gpw', '', '', `"useitem,sock=1"`, '', '', '', '', '');
+    PushData(`Armo Sock Crf`, '', 2, `"armo,crf,nos"`, 'gpw', '', '', `"useitem,sock=1"`, '', '', '', '', '');
+}
+
+// 白色装备+解毒药剂=(无形)白色装备
+// useitem代表装备部位和属性都不变(但是测试无形时会出BUG装备被洗成斧头并且不是无形)
+// usetype代表只有装备部位不变
+// 因为测试中发现各种问题,比如超强的15ed等属性会被随机掉,黄装的属性也会被洗掉,带孔也是属性所以会被洗掉
+// 所以最后只能支持不带孔的非超强白装
+if (config.eth_norItem)
+{
+    PushData(`Eth Weap Nor Item`, '', 2, `"weap,nor,nos,noe"`, 'yps', '', '', `"usetype,nor,eth"`, '', 100, '', '', '');
+    PushData(`Eth Armo Nor Item`, '', 2, `"armo,nor,nos,noe"`, 'yps', '', '', `"usetype,nor,eth"`, '', 100, '', '', '');
+}
+
+// 宝石转换颜色
+// 3个字母含意
+// 1 g = gem 固定不变
+// 2 c 碎裂的 f 瑕疵的 s 普通的 l 无暇的 p 完美的
+// 3 v 紫 y 黄 b 蓝 g 绿 r 红 w 白
+// Chipped 碎裂的
+if (config.gem_switch)
+{
+    PushData(`Chipped Amet -> Chipped Topa`, '', 1, 'gcv', '', '', '', 'gcy', '', '', '', '', '');
+    PushData(`Chipped Topa -> Chipped Sapp`, '', 1, 'gcy', '', '', '', 'gcb', '', '', '', '', '');
+    PushData(`Chipped Sapp -> Chipped Emer`, '', 1, 'gcb', '', '', '', 'gcg', '', '', '', '', '');
+    PushData(`Chipped Emer -> Chipped Ruby`, '', 1, 'gcg', '', '', '', 'gcr', '', '', '', '', '');
+    PushData(`Chipped Ruby -> Chipped Diam`, '', 1, 'gcr', '', '', '', 'gcw', '', '', '', '', '');
+    PushData(`Chipped Diam -> Chipped Amet`, '', 1, 'gcw', '', '', '', 'gcv', '', '', '', '', '');
+
+    // Flawed 瑕疵的
+    PushData(`Flawed Amet -> Flawed Topa`, '', 1, 'gfv', '', '', '', 'gfy', '', '', '', '', '');
+    PushData(`Flawed Topa -> Flawed Sapp`, '', 1, 'gfy', '', '', '', 'gfb', '', '', '', '', '');
+    PushData(`Flawed Sapp -> Flawed Emer`, '', 1, 'gfb', '', '', '', 'gfg', '', '', '', '', '');
+    PushData(`Flawed Emer -> Flawed Ruby`, '', 1, 'gfg', '', '', '', 'gfr', '', '', '', '', '');
+    PushData(`Flawed Ruby -> Flawed Diam`, '', 1, 'gfr', '', '', '', 'gfw', '', '', '', '', '');
+    PushData(`Flawed Diam -> Flawed Amet`, '', 1, 'gfw', '', '', '', 'gfv', '', '', '', '', '');
+
+    // 普通的
+    PushData(`Amet -> Topa`, '', 1, 'gsv', '', '', '', 'gsy', '', '', '', '', '');
+    PushData(`Topa -> Sapp`, '', 1, 'gsy', '', '', '', 'gsb', '', '', '', '', '');
+    PushData(`Sapp -> Emer`, '', 1, 'gsb', '', '', '', 'gsg', '', '', '', '', '');
+    PushData(`Emer -> Ruby`, '', 1, 'gsg', '', '', '', 'gsr', '', '', '', '', '');
+    PushData(`Ruby -> Diam`, '', 1, 'gsr', '', '', '', 'gsw', '', '', '', '', '');
+    PushData(`Diam -> Amet`, '', 1, 'gsw', '', '', '', 'gsv', '', '', '', '', '');
+
+    // Flawless 无暇的
+    PushData(`Flawless Amet -> Flawless Topa`, '', 1, 'glv', '', '', '', 'gly', '', '', '', '', '');
+    PushData(`Flawless Topa -> Flawless Sapp`, '', 1, 'gly', '', '', '', 'glb', '', '', '', '', '');
+    PushData(`Flawless Sapp -> Flawless Emer`, '', 1, 'glb', '', '', '', 'glg', '', '', '', '', '');
+    PushData(`Flawless Emer -> Flawless Ruby`, '', 1, 'glg', '', '', '', 'glr', '', '', '', '', '');
+    PushData(`Flawless Ruby -> Flawless Diam`, '', 1, 'glr', '', '', '', 'glw', '', '', '', '', '');
+    PushData(`Flawless Diam -> Flawless Amet`, '', 1, 'glw', '', '', '', 'glv', '', '', '', '', '');
+
+    // Perfect 完美的
+    PushData(`Perfect Amet -> Perfect Topa`, '', 1, 'gpv', '', '', '', 'gpy', '', '', '', '', '');
+    PushData(`Perfect Topa -> Perfect Sapp`, '', 1, 'gpy', '', '', '', 'gpb', '', '', '', '', '');
+    PushData(`Perfect Sapp -> Perfect Emer`, '', 1, 'gpb', '', '', '', 'gpg', '', '', '', '', '');
+    PushData(`Perfect Emer -> Perfect Ruby`, '', 1, 'gpg', '', '', '', 'gpr', '', '', '', '', '');
+    PushData(`Perfect Ruby -> Perfect Diam`, '', 1, 'gpr', '', '', '', 'gpw', '', '', '', '', '');
+    PushData(`Perfect Diam -> Perfect Amet`, '', 1, 'gpw', '', '', '', 'gpv', '', '', '', '', '');
+}
+
+// 洗板子只需要1瓶大紫
+if (config.roll_mag)
+{
+    cubemain.rows.forEach((row) =>
+    {
+        if (row['input 2'] === `"gem4,qty=3"`)
+        {
+            row['numinputs'] = 2;
+            row['input 2'] = 'rvl';
+        }
+    });
+}
+
+// 洗板子只需要1瓶大紫
+if (config.roll_rar)
+{
+    PushData('Re-rolled Rare Item', '', 2, `"any,rar"`, 'rvl', '', '', `"usetype,rar"`, '', 100, '', '', '');
+}
+
+// 添加数据函数
+function PushData(des, op, numinputs, input1, input2, input3, input4, output, plvl, ilvl, mod1, mod1min, mod1max)
+{
+    cubemain.rows.push({
+        'description': des,
+        'enabled': 1,
+        'version': 100,
+        'op': op,
+        'numinputs': numinputs,
+        'input 1': input1,
+        'input 2': input2,
+        'input 3': input3,
+        'input 4': input4,
+        'output': output,
+        'plvl': plvl,
+        'ilvl': ilvl,
+        'mod 1': mod1,
+        'mod 1 min': mod1min,
+        'mod 1 max': mod1max,
+        '*eol\r': 0
+    });
+}
+
+D2RMM.writeTsv(cubemainFileName, cubemain);
