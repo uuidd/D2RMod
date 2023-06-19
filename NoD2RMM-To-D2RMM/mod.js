bb3da4c0d87c1d5cb0395a8de57bad9deeaf74b2
demo <liyihuiid@gmail.com>
2023-06-19 15:37:00 +0800

    删除部分组件

--- NoD2RMM-To-D2RMM/mod.js
+++
@@ -1,9 +0,0 @@
-if (D2RMM.getVersion == null || D2RMM.getVersion() < 1.4) {
-  D2RMM.error('Requires D2RMM version 1.4 or higher.');
-  return;
-}
-D2RMM.copyFile(
-  'MyMod', // <mod folder>
-  '', // <diablo 2 folder>\mods\<modname>\<modname>.mpq\data
-  true // overwrite any conflicts
-);
--- yte mod/mod.js
+++
@@ -1,5 +0,0 @@
-D2RMM.copyFile(
-  'data', // <mod folder>
-  '', // <diablo 2 folder>\mods\<modname>\<modname>.mpq\data
-  true // overwrite any conflicts
-);
