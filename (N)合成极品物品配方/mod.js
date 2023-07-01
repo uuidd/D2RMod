d53acfb4b79ebc8c8731cffbf9f124c7c9b7b688
demo <1942125235@qq.com>
2023-07-01 22:44:58 +0800

    添加马拉乔丹合成公式

--- (N)合成极品物品配方/mod.js
+++ (N)合成极品物品配方/mod.js
@@ -1,17 +1,23 @@
 const cubemainFilename = "global\\excel\\cubemain.txt";
 const cubemain = D2RMM.readTsv(cubemainFilename);
-// 做符合原版的极品物品
+// 做符合原版的极品物品 Make high-quality items that match the original version
+
 // pre= 为 global\excel\magicprefix.txt 对应词缀行号-2
 // suf= 为 global\excel\magicsuffix.txt 对应词缀行号-2
-const jewRecipe = {
-  description: 'Magic jew - Increased Attack Speed 15',
+const baseRecipe = {
   enabled: 1,
   version: 100,
-  numinputs: 3,
-  'input 1': `"jew,rar"`,
   plvl: 100,
   '*eol\r': 0,
 };
+
+const jewRecipe = {
+  ...baseRecipe,
+  description: 'Magic jew - Increased Attack Speed 15',
+  numinputs: 3,
+  'input 1': `"jew,rar"`,
+};
+
 // 15攻速31-40ed蓝色珠宝(红白珠)
 if (config.jew15sp40ed) {
   cubemain.rows.push({
@@ -44,4 +50,73 @@ if (config.jew15sp30fire) {
     output: `"jew,mag,pre=376,suf=171"`
   });
 }
+
+// 合成马拉
+if (config.mara3) {
+  cubemain.rows.push({
+    ...baseRecipe,
+    description: `3 unique Amulet = Mara's Kaleidoscope`,
+    numinputs: 3,
+    'input 1': 'amu,uni,qty=3',
+    output: `Mara's Kaleidoscope`
+  });
+}
+
+if (config.mara26) {
+  cubemain.rows.push({
+    ...baseRecipe,
+    description: `r26 + 1 Amulet = Mara's Kaleidoscope`,
+    numinputs: 2,
+    'input 1': 'amu',
+    'input 2': 'r26',
+    output: `Mara's Kaleidoscope`
+  });
+}
+
+if (config.mara32) {
+  cubemain.rows.push({
+    ...baseRecipe,
+    description: `r32 + 1 Amulet = Mara's Kaleidoscope`,
+    numinputs: 2,
+    'input 1': 'amu',
+    'input 2': 'r32',
+    output: `Mara's Kaleidoscope`
+  });
+}
+
+// 合成乔丹
+if (config.soj3) {
+  cubemain.rows.push({
+    ...baseRecipe,
+    description: `3 unique ring = The Stone of Jordan`,
+    numinputs: 3,
+    'input 1': 'rin,uni,qty=3',
+    output: `The Stone of Jordan`
+  });
+}
+
+if (config.soj28) {
+  cubemain.rows.push({
+    ...baseRecipe,
+    description: `r28 + ring = The Stone of Jordan`,
+    numinputs: 2,
+    'input 1': 'rin',
+    'input 2': 'r28',
+    output: `The Stone of Jordan`
+  });
+}
+
+if (config.soj33) {
+  cubemain.rows.push({
+    ...baseRecipe,
+    description: `r33 + ring = The Stone of Jordan`,
+    numinputs: 2,
+    'input 1': 'rin',
+    'input 2': 'r33',
+    output: `The Stone of Jordan`
+  });
+}
+
+
+
 D2RMM.writeTsv(cubemainFilename, cubemain);
