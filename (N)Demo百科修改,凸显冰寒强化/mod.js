443e5280e07787c2720212270bcf8563052f8256
demo <1942125235@qq.com>
2023-07-11 00:12:47 +0800

    新增百科

--- (N)Demo百科修改,凸显冰寒强化/mod.js
+++ (N)Demo百科修改,凸显冰寒强化/mod.js
@@ -1,6 +1,6 @@
-const npcsFilename = 'local\\lng\\strings\\npcs.json';
-const npcs = D2RMM.readJson(npcsFilename);
-if (config.baike) {
+if (config.demoInfo) {
+  const npcsFilename = 'local\\lng\\strings\\npcs.json';
+  const npcs = D2RMM.readJson(npcsFilename);
   npcs.forEach((item) => {
     const itemKey = item.Key;
     let twName = null;
@@ -28,19 +28,67 @@ if (config.baike) {
       item['zhCN'] = cnName;
     }
   });
+  D2RMM.writeJson(npcsFilename, npcs);
 }
 
-D2RMM.writeJson(npcsFilename, npcs);
 
 // 凸显冰寒强化文本
-const monstersFilename = 'local\\lng\\strings\\monsters.json';
-const monsters = D2RMM.readJson(monstersFilename);
+
 if (config.coldText) {
+  const monstersFilename = 'local\\lng\\strings\\monsters.json';
+  const monsters = D2RMM.readJson(monstersFilename);
   monsters.forEach((item) => {
     if (item.Key === 'monsteruniqueprop1') {
       item['zhTW'] = 'ÿcN╪冰寒強化';
       item['zhCN'] = 'ÿcN╪冰霜强化';
     }
   });
+  D2RMM.writeJson(monstersFilename, monsters);
 }
-D2RMM.writeJson(monstersFilename, monsters);
+
+
+// 查询 Demo 百科
+if (config.demoSelect) {
+  // 暂停 退出并保存游戏界面
+  const pauselayouthdFilename = 'global\\ui\\layouts\\pauselayouthd.json';
+  const pauselayouthd = D2RMM.readJson(pauselayouthdFilename);
+  pauselayouthd.children.forEach((item) => {
+    if (item.name === 'PauseTable') {
+      const demoMod = {
+        "type": "TableRowWidget",
+        "name": "Demo Select",
+        "children": [
+          {
+            "type": "ButtonWidget",
+            "name": "Settings",
+            "fields": {
+              "filename": "PauseMenu\\PauseButton",
+              "hoveredFrame": 3,
+              "textString": "DemoMod 2.6", // 帮助页名称
+              "onClickMessage": "PanelManager:OpenPanel:demopanel", // 点击动作开启的的页面，对应global\ui\layouts\{}hd.json
+              "textColor": "$FontColorOrange", // 字体颜色
+              "text/style": "$StyleFEButtonText",
+              "acceptsReturnKey": true,
+              "focusOnMouseOver": true,
+              "sound": "select"
+            }
+          }
+        ]
+      }
+      item.children.unshift(demoMod);
+    }
+  });
+  D2RMM.writeJson(pauselayouthdFilename, pauselayouthd);
+  // 复制组件下的global下json文件到mod目录
+  //
+  D2RMM.copyFile('global', 'global', true);
+  const demopanelhdFilename = 'global\\ui\\layouts\\demopanelhd.json'
+  const demopanelhd = D2RMM.readJson(demopanelhdFilename);
+  demopanelhd.children[0].children[1].children[1].fields.text = "DemoMod 2.6" // 帮助页面大标题
+  demopanelhd.children[2].fields.textStrings = ['合成公式', '手工品配方', '模拟联机交易','版本变更'] // 必须四个
+  D2RMM.writeJson(demopanelhdFilename, demopanelhd);
+
+
+}
+
+
