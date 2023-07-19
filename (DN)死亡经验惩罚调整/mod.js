e1e91902c1c726318f210d2fdda1296e40123157
demo <1942125235@qq.com>
2023-07-19 21:00:50 +0800

    名称大修改

---
+++ (DN)死亡经验惩罚调整/mod.js
@@ -0,0 +1,15 @@
+// 去掉死亡时的经验惩罚
+if (config.nightmare !== 5 && config.hell !== 10) {
+  const difficultylevelsFilename = 'global\\excel\\difficultylevels.txt';
+  const difficultylevels = D2RMM.readTsv(difficultylevelsFilename);
+  difficultylevels.rows.forEach((row) => {
+    if (row['Name'] === 'Nightmare') {
+      row['DeathExpPenalty'] = config.nightmare;
+    } else if (row['Name'] === 'Hell') {
+      row['DeathExpPenalty'] = config.hell;
+    }
+  });
+  D2RMM.writeTsv(difficultylevelsFilename, difficultylevels);
+}
+
+
---
+++ (ZN)修复设置界面字体大小/mod.js
@@ -0,0 +1,18 @@
+// Character selection panel is in characterselectpanelhd.json
+// but can't disable anything in there without affecting singleplayer
+// and the tab control's enabled/disabled state is controlled by code
+// not by ui config
+
+if (config.fixFont) {
+  const profileHDFilename = 'global\\ui\\layouts\\_profilehd.json';
+  const profileHD = D2RMM.readJson(profileHDFilename);
+  profileHD.StyleSettingsNumericSlider = {
+    ...profileHD.StyleSettingsNumeric,
+    alignment: {
+      ...profileHD.StyleSettingsNumeric.alignment,
+      h: 'right',
+    },
+  };
+  profileHD.SettingsSliderValueFields.style = '$StyleSettingsNumericSlider';
+  D2RMM.writeJson(profileHDFilename, profileHD);
+}
