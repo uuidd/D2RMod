c8afd1525b97d3df864e384b1e749c9ea049931d
demo <1942125235@qq.com>
2023-07-30 02:50:33 +0800

    初步完成迷你盒子

--- (IN)迷你方盒/mod.js
+++ (IN)迷你方盒/mod.js
@@ -5,14 +5,14 @@ const helpPanelHDFilename = 'global\\ui\\layouts\\helppanelhd.json';
 let helpPanelHD = D2RMM.readJson(helpPanelHDFilename);
 const helpMiniCube = {
   "type": "TextBoxWidget", "name": "HelpMiniCube", "fields": {
-    "rect": {"x": 1385, "y": 1405}, "text": "迷你方塊",//迷你方塊
+    "rect": {"x": 1385, "y": 1405}, "text": "迷你盒子",//帮助页指示迷你盒子
     "fontType": "12ptF", "style": {
       "fontColor": "$FontColorGoldYellow", "pointSize": "$MediumLargeFontSize", "alignment": {"h": "center"}
     }
   }
 }
 const helpMiniCubeLine = {
-  "type": "IndicatorLineWidget", "name": "MiniCubeLine",//迷你方塊指示綫
+  "type": "IndicatorLineWidget", "name": "MiniCubeLine",//迷你盒子指示綫
   "fields": {
     "rect": {"x": 1382, "y": 1475, "width": 6, "height": 290}, "color": [1, 1, 1, 1]
   }
@@ -31,7 +31,7 @@ const miniCubeIcon = {
       "x": 2750, "y": 316, "scale": 0.8
     },
     "filename": "panel\\hud_02\\minicubeicon",
-    "tooltipString": "開啓/關閉\nÿc2迷你方塊",
+    "tooltipString": "开启/关闭\n迷你盒子",
     "onClickMessage": "PanelManager:TogglePanel:MiniCubeLayout",
     "focusIndicatorVisible": true,
     "hoveredFrame": 2,
